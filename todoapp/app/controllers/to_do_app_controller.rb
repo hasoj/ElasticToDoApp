b3805c0655795f4f439738e1ae567b093ba2a46a
Dan Serban <dserban01@yahoo.com>
2016-10-16 15:59:47 +0300

    Latest changes

--- todoapp/app/controllers/to_do_app_controller.rb
+++ todoapp/app/controllers/to_do_app_controller.rb
@@ -18,10 +18,67 @@ class ToDoAppController < ActionController::Base
       index :mynotesindex
       type  :note
     end
-    @allitems = repository.search('*').to_a.map(&:to_hash)
-    @allitems.each do |item|
-      p item
+
+    case session[:what_to_show]
+      when 'show_completed'
+        @allitems = repository.search('done:true').to_a.map(&:to_hash)
+      when 'show_active'
+        @allitems = repository.search('done:false').to_a.map(&:to_hash)
+      else
+        @allitems = repository.search('*').to_a.map(&:to_hash)
     end
+
   end
+
+  def withitem_do
+    repository = Elasticsearch::Persistence::Repository.new do
+      client Elasticsearch::Client.new url: 'http://localhost:9200', log: true
+      index :mynotesindex
+      type  :note
+    end
+
+    item = repository.find(params[:id].to_i).to_hash
+    item['done'] = !item['done']
+    repository.save(item)
+
+    puts
+    puts
+    puts
+    p item
+    puts
+    puts
+    puts
+
+    item = repository.find(params[:id].to_i).to_hash
+    
+    puts
+    puts
+    puts
+    p item
+    puts
+    puts
+    puts
+
+    redirect_to '/todo'
+  end
+
+  def buttonsatbottom
+    repository = Elasticsearch::Persistence::Repository.new do
+      client Elasticsearch::Client.new url: 'http://localhost:9200', log: true
+      index :mynotesindex
+      type  :note
+    end
+    
+    session['what_to_show'] = 'show_all'
+    if params.has_key?('buttontoshowcompleted')
+      session['what_to_show'] = 'show_completed'
+    end
+    if params.has_key?('buttontoshowactive')
+      session['what_to_show'] = 'show_active'
+    end
+
+    redirect_to '/todo'
+  end
+
 end
 
--- todoapp/config/routes.rb
+++ todoapp/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/todo' => 'to_do_app#todo'
+
+  post '/withitem/:id' => 'to_do_app#withitem_do'
+  post '/buttonsatbottom' => 'to_do_app#buttonsatbottom'
 end
